14624787fe84a01a32643f222174990dce8e4492
lottiealdridge <lottiealdridge95@gmail.com>
2024-11-02 21:39:56 +0000

    added hint, inproved JS and added blink CSS

--- src/index.js
+++ src/index.js
@@ -17,24 +17,16 @@ function generatePoem(event) {
     let prompt = `User instructions: Generate a french poem about ${instructionsInput.value}`;
     let context = `You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Sign off the poem with "SheCodes AI" wrapped in a <strong> element. Do not add a title. Make sure to follow the user instructions.`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    let poemElement = document.querySelector("#poem");
+    
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="blink">⌛Generating a French poem about ${instructionsInput.value}</div>`;
 
     console.log("generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context:${context}`);
 
     axios.get(apiURL).then(displayPoem);
-    //display generated poem
-
-
-
-
-    let poemElement = document.querySelector("#poem");
-    new Typewriter('#poem', {
-        strings: "The poem will be posted here",
-        autoStart: true,
-        delay: 1,
-        cursor:""
-      });
 
 }
 
